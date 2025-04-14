108a88ef653d13a10a5d2b0feaeb7599501bc607
jayasri0710 <jayasrim0710@gmail.com>
2025-04-14 21:38:48 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        String text = scan.next();
+        char ch = scan.next().charAt(0);
+        System.out.println(num);
+        System.out.println(text);
+        System.out.println(ch);
+
+    }
+}
