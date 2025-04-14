334fee2d1e01f423266ade1b278623d90484ab9d
jayasri0710 <jayasrim0710@gmail.com>
2025-04-14 22:35:24 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the student's mark: "); 
+        int a = scan.nextInt();
+
+        if (a >= 90) {
+            System.out.print("Grade A");
+        } else if (a >= 80) {
+            System.out.print("Grade B");
+        } else if (a >= 60) {
+            System.out.print("Grade C");
+        } else if (a >= 35) {
+            System.out.print("Grade D");
+        } else {
+            System.out.print("Fail");
+        }
+
+        
+    }
+}
